dd411ffe9827fa4531301f1c98453f8e4e92fd55
Tristan A. <tristanagraa@gmail.com>
2024-11-07 17:29:03 +0700

    refactor: rename path

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { social } from "./constant";
+import { social } from "./const";
 import { MapPin, Mail } from "lucide-react";
 
 export const Footer = () => {
