627dcdf27f67adced0efc04eb2c32469499f6a18
Tristan A. <tristanagraa@gmail.com>
2024-11-10 18:19:08 +0700

    feat: preview component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
-import Image from "next/image";
-import { Loading } from "@/components/elements/Loading";
+"use client";
+import { Button } from "@/components/ui/button";
+import Chip from "@/components/ui/chip";
+import Countdown from "@/components/ui/countdown";
+import Pagination from "@/components/ui/pagination";
+import Tooltip from "@/components/ui/tooltip";
+import { useState } from "react";
 
 export default function Home() {
+  const [page, setPage] = useState(1);
   return (
-    <div className="">
-      <Loading />
+    <div className="min-h-screen flex justify-center flex-col gap-6 items-center">
+      <div className="grid grid-cols-2 gap-6">
+        <Button variant="primary">Click me</Button>
+        <Button isLoading variant="primary">
+          Click me
+        </Button>
+        <Button variant="secondary">Click me</Button>
+        <Button isLoading variant="secondary">
+          Click me
+        </Button>
+        <Button variant="tertiary">Click me</Button>
+        <Button isLoading variant="tertiary">
+          Click me
+        </Button>
+        <Button variant="destructive">Click me</Button>
+        <Button isLoading variant="destructive">
+          Click me
+        </Button>
+      </div>
+      <Countdown displayDate targetDate={new Date("2024-12-31T23:59:59")} />
+      <Chip>Chip</Chip>
+      <Chip variant="secondary">Chip</Chip>
+      <Chip variant="tertiary">Chip</Chip>
+      <Button variant="primary">
+        <Tooltip text="Tooltip">Hover me</Tooltip>
+      </Button>
+      <Pagination currentPage={page} totalPages={10} onPageChange={setPage} />
     </div>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,56 +3,37 @@ import { cn } from "@/lib/utils";
 import {
   ButtonHTMLAttributes,
   forwardRef,
-  ForwardRefExoticComponent,
-  RefAttributes,
   useState,
-  createElement,
 } from "react";
-import { Loader2, LucideProps } from "lucide-react";
+import { Loader2 } from "lucide-react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary" | "tertiary" | "destructive";
   isLoading?: boolean;
-  icon?: ForwardRefExoticComponent<
-    Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
-  >;
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    {
-      className,
-      variant = "primary",
-      icon,
-      isLoading,
-      children,
-      disabled,
-      ...props
-    },
+    { className, variant = "primary", isLoading, children, disabled, ...props },
     ref
   ) => {
-    const [clicked, setClicked] = useState(false);
-
-    const handleClick = () => {
-      setClicked(!clicked);
-    };
 
     const buttonClasses = `
     inline-flex items-center justify-center rounded-[1.25rem] 
     disabled:pointer-events-none disabled:opacity-70 
-    font-openSans text-base leading-6 px-7 py-3
-    md:text-lg md:px-8 md:py-4
-    lg:text-xl lg:leading-7 lg:px-9 lg:py-5 transition-all duration-200 ease-in-out
+    font-openSans text-base leading-6 px-8 py-3
+    md:text-lg md:px-9 md:py-4
+    lg:text-xl lg:leading-7 transition-all duration-300 ease-in-out
     `;
 
     const variantStyles = {
       primary:
-        "bg-gradient-to-b from-magenta-500 to-magenta-button text-white hover:bg-gradient-to-b hover:from-magenta-button-hover hover:to-magenta-500",
+        "bg-gradient-to-b from-magenta-500 to-magenta-button text-white hover:bg-gradient-to-b hover:from-magenta-button-hover hover:to-magenta-500 active:bg-gradient-to-b active:from-magenta-700 active:to-magenta-button-active",
       secondary:
-        "bg-magenta-50 text-magenta-600 hover:bg-magenta-100 border-2 border-magenta-700",
+        "bg-magenta-50 text-magenta-600 hover:bg-magenta-100 border-2 border-magenta-700 active:bg-magenta-200",
       tertiary:
-        "bg-white text-magenta-600 hover:bg-magenta-50 border-2 border-magenta-200",
-      destructive: "bg-red-400 text-white hover:bg-red-500",
+        "bg-white text-magenta-600 hover:bg-magenta-50 border-2 border-[#DEA3C7] active:bg-magenta-100",
+      destructive: "bg-red-400 text-white hover:bg-red-500 active:bg-red-600",
     };
 
     const disabledVariantStyles = {
@@ -62,24 +43,15 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       destructive: "bg-red-100 text-white",
     };
 
-    const clickedVariantStyles = {
-      primary: "bg-gradient-to-b from-magenta-700 to-magenta-button-active",
-      secondary: "bg-magenta-200",
-      tertiary: "bg-magenta-100",
-      destructive: "bg-red-400",
-    };
-
     return (
       <button
         ref={ref}
         className={cn(
           buttonClasses,
           disabled ? disabledVariantStyles[variant] : variantStyles[variant],
-          clicked ? clickedVariantStyles[variant] : "",
           className
         )}
         disabled={disabled || isLoading}
-        onClick={handleClick}
         {...props}
       >
         {isLoading ? (
@@ -90,9 +62,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           </>
         ) : (
           <>
-            {icon && <span className="mr-2">{createElement(icon)}</span>}
             <span className="flex justify-center w-full">{children}</span>
-            {icon && <span className="ml-2">{createElement(icon)}</span>}
           </>
         )}
       </button>
