3b810d0140952574261d942b4b89ffff5e42c456
secona <77039267+secona@users.noreply.github.com>
2024-11-10 21:46:50 +0700

    feat: input checkbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import { Loading } from "@/components/elements/Loading";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
   return (
-    <div className="">
-      <Loading />
+    <div className="mt-56">
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <label
+          htmlFor="terms"
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          Accept terms and conditions
+        </label>
+      </div>
     </div>
   );
 }
