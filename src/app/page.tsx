48b0c3af151741abf47d15efd8ba72a3fbc8601a
secona <77039267+secona@users.noreply.github.com>
2024-11-10 22:22:31 +0700

    feat: combobox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 import Image from "next/image";
 import { Loading } from "@/components/elements/Loading";
-import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
   return (
-    <div className="mt-56">
-      <div className="flex items-center space-x-2">
-        <Checkbox id="terms" />
-        <label
-          htmlFor="terms"
-          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-        >
-          Accept terms and conditions
-        </label>
-      </div>
+    <div className="">
+      <Loading />
     </div>
   );
 }
