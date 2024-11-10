1fd94eddb5b1c84ed75f403026786689a1c90e4e
Tristan A. <tristanagraa@gmail.com>
2024-11-11 00:53:45 +0700

    fix: 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,20 +5,22 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <main className="min-h-screen flex flex-col justify-center items-center text-center">
-      <h1 className="font-cinzel text-[160px] md:text-[200px] lg:text-[288px] text-violet-900/40">
+      <h1 className="font-cinzel text-[96px] md:text-[200px] lg:text-[288px] text-violet-900/40">
         404
       </h1>
-      <h2 className="font-cinzel text-t5 md:text-t3 lg:text-t2">
-        Oops! Something&apos;s went wrong
-      </h2>
-      <h3 className="font-raleway text-t7 md:text-t6 lg:text-t5 max-md:mt-3">
-        seems like the page you&apos;re trying to open is not ready
-      </h3>
-      <Link href={"/"}>
-        <Button variant="primary" className="mt-8">
-          Back to Home
-        </Button>
-      </Link>
+      <div className="lg:-translate-y-20 md:-translate-y-16 -translate-y-2">
+        <h2 className="font-cinzel text-t6 md:text-t3">
+          Oops! Something&apos;s went wrong
+        </h2>
+        <h3 className="font-raleway text-t8 md:text-t6 max-md:mt-3">
+          seems like the page you&apos;re trying to open is not ready
+        </h3>
+        <Link href={"/"}>
+          <Button variant="primary" className="mt-8">
+            Back to Home
+          </Button>
+        </Link>
+      </div>
     </main>
   );
 };
