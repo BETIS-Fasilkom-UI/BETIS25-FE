e32f7dc54174e692ed33c78ce63d00ef55ebcc6e
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-14 11:03:27 +0700

    feat: apply icon and supress hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body
+        suppressContentEditableWarning
+        suppressHydrationWarning
         className={` ${raleway.variable} ${cinzel.variable} ${openSans.variable} antialiased`}
       >
         <NextTopLoader showSpinner={false} color="#c5599e" shadow={false} />
