212a95863db53610841fed725ee155542c7f5121
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2024-12-23 19:43:54 +0700

    feat: Added Timeline Background

---
+++ src/modules/TimelineModule/index.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+
+const TimelineModule = () => {
+    return (
+        <div 
+            className="min-w-screen min-h-[209.028vw] mt-[110px]" 
+            style={{
+                'backgroundImage': `url('/TimelineBg.png')`, 
+                'backgroundSize': 'cover', 
+                'backgroundRepeat': 'no-repeat'
+            }}
+        >
+            Hola
+        </div>
+    )
+}
+
+export default TimelineModule
