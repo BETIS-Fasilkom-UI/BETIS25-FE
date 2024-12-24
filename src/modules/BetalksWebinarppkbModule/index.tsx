33520814d04c0ada3e2453358a308882f8759bcb
Daffai06 <daffa.ismail178@gmail.com>
2024-12-24 17:36:41 +0700

    Responsive for below lg and lg. Messy padding

--- src/modules/BetalksWebinarppkbModule/index.tsx
+++ src/modules/BetalksWebinarppkbModule/index.tsx
@@ -1,51 +1,44 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
 import Image from "next/image";
 
 const BetalksWebinarppkbModule = () => {
   return (
-    <div>
-      <div className="flex w-[1328px] h-[861.24px] flex-col justify-start items-start gap-3">
-        <div className="text-[var(--Font,#FEF5FF)] font-cinzel text-5xl font-black leading-[60px] ">
-          Our Event
-        </div>
-
-        <div className="flex flex-col items-start gap-[144px] self-stretch">
-          <Card className="flex flex-row-reverse w-[1278px] h-[300px] py-[28px] px-[24px]">
-            <div className="flex relative w-[480px] h-[307px] flex-shrink-0">
-              <Image
-                alt="Megaphone"
-                src="/imageBEtalks.png"
-                fill
-                sizes="none"
-                className="object-contain"
-              />
-            </div>
-            <div className="flex w-[729px] h-[236px] flex-col items-start gap-[20px] flex-shrink-0">
-              <div className="text-[var(--Font,#FEF5FF)] font-cinzel text-[36px] font-[800] leading-[40px]">
-                be talks
-              </div>
-
-              <div className="text-[var(--Font,#FEF5FF)] text-justify font-raleway text-[16px] font-[600] leading-[28px]">
-                Be-Talks adalah acara yang bertujuan untuk membangkitkan
-                semangat belajar bagi peserta BETIS Fasilkom UI 2025, serta
-                masyarakat umum agar tetap konsisten dalam mengejar impian
-                mereka. Dengan adanya Be-Talks, diharapkan peserta dapat kembali
-                termotivasi dan lebih bersemangat dalam belajar
-              </div>
-
-              <div className="h-12 justify-start items-start gap-3 inline-flex">
-                <Button className="flex-grow">Daftar Sekarang</Button>
-                <Button className="flex-grow" variant="secondary">
-                  Description
-                </Button>
-              </div>
-            </div>
-          </Card>
-        </div>
+    <div className="flex flex-col mx-auto">
+      <div className="text-[var(--Font,#FEF5FF)] font-cinzel text-3xl font-black leading-[60px] mb-5 xl:flex xl:text-[3.2rem] xl:mb-12">
+        Our Event
       </div>
+
+      <Card className="!flex !flex-col-reverse px-7 mb-10 xl:!pl-14 xl:!pr-0 xl:!py-9 xl:!h-[320px] xl:!flex-row ">
+        <div className="w-[318px] xl:w-[729px] flex flex-col">
+          <div className="text-[var(--Font,#FEF5FF)] font-cinzel text-3xl xl:text-4xl font-black leading-10 mt-8 xl:mt-0 xl:flex mb-4">
+            be talks
+          </div>
+          <div className="text-[var(--Font,#FEF5FF)] text-justify font-raleway text-sm xl:text-base font-medium leading-7 mb-5">
+            Be-Talks adalah acara yang bertujuan untuk membangkitkan semangat
+            belajar bagi peserta BETIS Fasilkom UI 2025, serta masyarakat umum
+            agar tetap konsisten dalam mengejar impian mereka. Dengan adanya
+            Be-Talks, diharapkan peserta dapat kembali termotivasi dan lebih
+            bersemangat dalam belajar
+          </div>
+          <div className="gap-3 flex flex-col xl:flex-row">
+            <Button className="flex-1 ">Daftar Sekarang</Button>
+            <Button className="flex-1 " variant="secondary">
+              Description
+            </Button>
+          </div>
+        </div>
+        <div className="relative w-[225px] h-[143px] flex mx-auto xl:w-[528px] xl:h-[337px] xl:!-top-28 xl:!-right-[60px]">
+          <Image
+            alt="Megaphone"
+            src="/imageBEtalks.png"
+            fill
+            sizes="none"
+            className="object-contain"
+          />
+        </div>
+      </Card>
     </div>
   );
 };
