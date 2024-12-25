9de0edd1553dd246d28981fff4ef13aff2c05e9e
Carleano Ravelza Wongso <carleanoravel@gmail.com>
2024-12-25 16:41:56 +0700

    update layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { Navbar } from './Navbar'
-import { Footer } from './Footer'
-import { Toaster } from '../ui/sonner';
+import React from "react";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
+import { Toaster } from "../ui/sonner";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-      <main className="min-h-screen font-openSans">
-        <Toaster position="top-center" />
-        <Navbar />
-        <main className="min-h-screen">
-          {children}
-        </main>
-        <Footer />
-      </main>
-    );
-  };
+  return (
+    <main className="min-h-screen max-w-screen overflow-x-hidden font-openSans">
+      <Toaster position="top-center" />
+      <Navbar />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </main>
+  );
+};
+
