35f43b12e711ccbeb9db97add2418e0e88e6f5e9
DaffaI06 <daffa.ismail178@gmail.com>
2024-12-25 15:24:35 +0700

    feat: made responsive

--- src/modules/BetalksWebinarppkbModule/index.tsx
+++ src/modules/BetalksWebinarppkbModule/index.tsx
@@ -5,17 +5,17 @@ import Image from "next/image";
 
 const BetalksWebinarppkbModule = () => {
   return (
-    <div className="flex flex-col mx-auto">
-      <div className="text-[var(--Font,#FEF5FF)] font-cinzel text-3xl font-black leading-[60px] mb-5 xl:flex xl:text-[3.2rem] xl:mb-12">
+    <div className="flex flex-col">
+      <div className="text-[var(--Font,#FEF5FF)] font-cinzel text-3xl font-black leading-[3.75rem] mb-5 xl:flex xl:text-[3.2rem] xl:mb-[3.25rem]">
         Our Event
       </div>
 
-      <Card className="!flex !flex-col-reverse px-7 mb-10 xl:!pl-14 xl:!pr-0 xl:!py-9 xl:!h-[320px] xl:!flex-row ">
-        <div className="w-[318px] xl:w-[729px] flex flex-col">
+      <Card className="!border-0 !shadow-[0px_0px_12px_rgba(255,255,255,0.60)] !flex !flex-col-reverse !px-8 !pt-2 !pb-12 !mb-10 xl:!pl-14 xl:!pr-0 xl:!pt-9 xl:!h-[300px] xl:!flex-row xl:mx-auto">
+        <div className="w-[72vmin] xl:w-[729px] flex flex-col">
           <div className="text-[var(--Font,#FEF5FF)] font-cinzel text-3xl xl:text-4xl font-black leading-10 mt-8 xl:mt-0 xl:flex mb-4">
             be talks
           </div>
-          <div className="text-[var(--Font,#FEF5FF)] text-justify font-raleway text-sm xl:text-base font-medium leading-7 mb-5">
+          <div className="text-[var(--Font,#FEF5FF)] text-justify font-raleway text-sm font-semibold xl:text-base xl:font-medium leading-7 mb-5 ">
             Be-Talks adalah acara yang bertujuan untuk membangkitkan semangat
             belajar bagi peserta BETIS Fasilkom UI 2025, serta masyarakat umum
             agar tetap konsisten dalam mengejar impian mereka. Dengan adanya
@@ -23,13 +23,13 @@ const BetalksWebinarppkbModule = () => {
             bersemangat dalam belajar
           </div>
           <div className="gap-3 flex flex-col xl:flex-row">
-            <Button className="flex-1 ">Daftar Sekarang</Button>
-            <Button className="flex-1 " variant="secondary">
+            <Button className="flex-1 !py-3">Daftar Sekarang</Button>
+            <Button className="flex-1 !py-3" variant="secondary">
               Description
             </Button>
           </div>
         </div>
-        <div className="relative w-[225px] h-[143px] flex mx-auto xl:w-[528px] xl:h-[337px] xl:!-top-28 xl:!-right-[60px]">
+        <div className="relative flex mx-auto w-[225px] h-[143px] md:w-[326px] md:h-[208px] xl:w-[528px] xl:h-[338px] xl:!-top-28 xl:!-right-[60px]">
           <Image
             alt="Megaphone"
             src="/imageBEtalks.png"
