2ab4405c56baf462791d1e65baa766dab91dc7e0
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2024-12-27 22:14:12 +0700

    fix: timeline background

--- src/modules/TimelineModule/index.tsx
+++ src/modules/TimelineModule/index.tsx
@@ -23,16 +23,19 @@ const TimelineModule = () => {
 
     return (
         <div
-            className="w-full h-[76.97vw] flex flex-col justify-center items-center relative mt-[10%]"
-            style={{
-                'backgroundImage': `url('/TimelineBg.png')`,
-                'backgroundSize': 'cover',
-                'backgroundRepeat': 'no-repeat'
-            }}
-        >   
+            className="w-full flex flex-col items-center pb-[30%]"
+        >
+            <div className="absolute w-full h-auto overflow-x-clip">
+                <div className="relative top-0 left-0">
+                    <div className="absolute z-100 min-w-[150vw] min-h-[100vw] top-[-12vw] left-[-25vw]">
+                        <Image src={'/TimelineBg.png'} alt=" " layout="fill" objectFit="contain" priority={true} />
+                    </div>
+                </div>
+            </div>
+
             {/* Treasure Chest, update the pt-[...] to adjust for the updated bg */}
-            <div className="flex-1 flex flex-col gap-0 items-center pt-[45%]"> 
-            {
+            <div className="flex-1 flex flex-col gap-0 items-center pt-[48%]">
+                {
                     isBigScreen ?
 
                         <div
@@ -61,11 +64,11 @@ const TimelineModule = () => {
                             <div className="z-10 absolute top-[-65px] left-[-33.5px] w-[67px] h-[70px]">
                                 <Image className="" src="/TreasureChest.png" alt="" width={67} height={70} loading="eager" />
                             </div>
-                            <SmallBlurredStarIcon className="absolute top-[-30px] left-[-37px]" />
+                            {/* <SmallBlurredStarIcon className="absolute top-[-30px] left-[-37px]" /> */}
                         </div>
                 }
-                </div>
-            
+            </div>
+
             {/* Timeline Section */}
             <div className="flex-1 flex flex-col gap-0 items-center bg-[#FCFFCC4D] rounded-[6px]">
                 {
@@ -77,15 +80,14 @@ const TimelineModule = () => {
                                     ${idx == events.length - 1 ? 'h-0' : 'md:h-[100px] md:border-[4.5px] lg:h-[140px] lg:border-[6.5px]'} 
                                     ${event.status === 'Done' ? 'border-[#FCFFCCCC]' : 'border-[#FCFFCC4D]'} 
                                     shadow-timeline relative 
-                                    ${(idx == events.length-1)||(events[idx+1].status==='Awaiting') ? 'rounded-b-[6px]' : ''}`}
+                                    ${(idx == events.length - 1) || (events[idx + 1].status === 'Awaiting') ? 'rounded-b-[6px]' : ''}`}
                             >
                                 {/* Horizontal line */}
                                 <div
-                                    className={`z-[-10] h-0
+                                    className={` h-0
                                         ${idx % 2 == 1 && 'right-0'} 
                                         md:w-[49px] lg:w-[70px]
-                                        ${idx == events.length - 1 ? 'md:top-[-4.5px] lg:top-[-6.5px]': 'md:top-[-8.5px] lg:top-[-12.5px]'}
-                                        
+                                        ${idx == events.length - 1 ? 'md:top-[-4.5px] lg:top-[-6.5px]' : 'md:top-[-8.5px] lg:top-[-12.5px]'}
                                         md:border-[2.25px] lg:border-[3.2px] 
                                         md:rounded-[1.4px] lg:rounded-[2px] 
                                         border-[#FCFFCCCC] 
@@ -108,13 +110,13 @@ const TimelineModule = () => {
                                     ${idx == events.length - 1 ? 'h-0' : 'h-[60px] border-[2.5px]'} 
                                     ${event.status === 'Done' ? 'border-[#FCFFCCCC]' : 'border-[#FCFFCC4D]'} 
                                     shadow-timeline relative
-                                    ${(idx == events.length-1)||(events[idx+1].status==='Awaiting') ? 'rounded-b-[2.5px]' : ''}`}
+                                    ${(idx == events.length - 1) || (events[idx + 1].status === 'Awaiting') ? 'rounded-b-[2.5px]' : ''}`}
                             >
                                 {/* Horizontal line */}
                                 <div
                                     className={`w-[27px] h-0 
                                         ${idx % 2 == 1 && 'right-0'} 
-                                        ${idx == events.length - 1 ? 'top-[-2px]': 'top-[-4px]'} 
+                                        ${idx == events.length - 1 ? 'top-[-2px]' : 'top-[-4px]'} 
                                         border-[1px] border-[#FCFFCCCC] rounded-[2.5px] 
                                         shadow-timeline absolute`}
                                 />
