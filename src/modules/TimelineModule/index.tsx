7cd6f456c4f368bc587dfb2762467abcd918de00
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2024-12-27 22:44:15 +0700

    fix: use css instead of useEffect for responsiveness

--- src/modules/TimelineModule/index.tsx
+++ src/modules/TimelineModule/index.tsx
@@ -1,28 +1,15 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { events } from "./const";
-import { BigEventCard, SmallEventCard } from "./module-elements/Card";
+import { EventCard } from "./module-elements/Card";
 import { BigBlurredStarIcon, SmallBlurredStarIcon } from "./module-elements/BlurredStarIcon";
 import Image from "next/image";
 
 
 const TimelineModule = () => {
-    const [isBigScreen, setIsBigScreen] = useState(false);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsBigScreen(window.innerWidth >= 768);
-        };
-
-        handleResize();
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
     return (
         <div
-            className="w-full flex flex-col items-center pb-[30%]"
+            className="w-full flex flex-col items-center pb-[45vh]"
         >
             <div className="absolute w-full h-auto overflow-x-clip">
                 <div className="relative top-0 left-0">
@@ -34,117 +21,86 @@ const TimelineModule = () => {
 
             {/* Treasure Chest, update the pt-[...] to adjust for the updated bg */}
             <div className="flex-1 flex flex-col gap-0 items-center pt-[45%]">
-                {
-                    isBigScreen ?
-
-                        <div
-                            key={`imagebruh`}
-                            className={`w-0 
-                                    md:h-[100px] md:border-[4.5px] lg:h-[140px] lg:border-[6.5px]
+                <div
+                    key={`imagebruh`}
+                    className={`w-0 
+                                    max-md:h-[60px] max-md:border-[2.5px]
+                                    md:h-[100px] md:border-[4.5px] 
+                                    lg:h-[140px] lg:border-[6.5px]
                                     border-[#FCFFCCCC]
                                     shadow-timeline relative`}
-                        >
-                            <div className={`z-10 absolute 
+                >
+                    <div className={`z-10 absolute 
+                                max-md:left-[-33.5px] max-md:top-[-65px] max-md:w-[67px] max-md:h-[70px]
                                 md:left-[-55px] md:top-[-102px] md:w-[105px] md:h-[110px] 
                                 lg:left-[-91.5px] lg:top-[-170px] lg:w-[175px] lg:h-[183px]`}>
-                                <Image className="" src="/TreasureChest.png" alt="" width={175} height={183} loading="eager" />
-                            </div>
-                            <BigBlurredStarIcon className="absolute top-[-100px] left-[-100px]" />
-                        </div>
-
-                        :
-                        <div
-                            key={`imagebruh`}
-                            className={`w-0 
-                                h-[60px] border-[2.5px]
-                                border-[#FCFFCCCC]
-                                shadow-timeline relative`}
-                        >
-                            <div className="z-10 absolute top-[-65px] left-[-33.5px] w-[67px] h-[70px]">
-                                <Image className="" src="/TreasureChest.png" alt="" width={67} height={70} loading="eager" />
-                            </div>
-                        </div>
-                }
+                        <Image className="" src="/TreasureChest.png" alt="" layout="fill" loading="eager" />
+                    </div>
+                    <div className="absolute max-md:hidden w-[125px] h-[125px] top-[-100px] left-[-100px]">
+                        <BigBlurredStarIcon  />
+                    </div>
+                </div>
             </div>
 
             {/* Timeline Section */}
             <div className="flex-1 flex flex-col gap-0 items-center bg-[#FCFFCC4D] rounded-[6px]">
                 {
-                    isBigScreen ?
-                        events.map((event, idx) =>
+                    events.map((event, idx) =>
+                        <div
+                            key={event.name}
+                            className={`w-0 
+                                ${idx == events.length - 1
+                                    ? 'h-0'
+                                    : 'max-md:h-[60px] max-md:border-[2.5px] md:h-[100px] md:border-[4.5px] lg:h-[140px] lg:border-[6.5px]'} 
+                                ${event.status === 'Done' ? 'border-[#FCFFCCCC]' : 'border-[#FCFFCC4D]'} 
+                                shadow-timeline relative 
+                                ${(idx == events.length - 1) || (events[idx + 1].status !== 'Done')
+                                    ? 'max-md:rounded-b-[2.5px] md:rounded-b-[6px]'
+                                    : ''}`}
+                        >
+                            {/* Horizontal line */}
                             <div
-                                key={event.name}
-                                className={`w-0 
-                                    ${idx == events.length - 1 ? 'h-0' : 'md:h-[100px] md:border-[4.5px] lg:h-[140px] lg:border-[6.5px]'} 
-                                    ${event.status === 'Done' ? 'border-[#FCFFCCCC]' : 'border-[#FCFFCC4D]'} 
-                                    shadow-timeline relative 
-                                    ${(idx == events.length - 1) || (events[idx + 1].status === 'Awaiting') ? 'rounded-b-[6px]' : ''}`}
+                                className={`h-0
+                                    ${idx % 2 == 1 && 'right-0'} 
+                                    max-md:w-[27px] md:w-[49px] lg:w-[70px]
+                                    ${idx == events.length - 1
+                                        ? 'max-md:top-[-2px] md:top-[-4.5px] lg:top-[-6.5px]'
+                                        : 'max-md:top-[-4px] md:top-[-8.5px] lg:top-[-12.5px]'}
+                                    max-md:border-[1px] max-md:rounded-[2.5px] 
+                                    md:border-[2.25px] md:rounded-[1.4px] 
+                                    lg:border-[3.2px] lg:rounded-[2px] 
+                                    border-[#FCFFCCCC] 
+                                    shadow-timeline absolute`}
+                            />
+                            <div 
+                                className={`absolute 
+                                    max-md:w-[45px] max-md:h-[37px] max-md:top-[-32px] max-md:left-[-37px]
+                                    md:w-[125px] md:h-[125px] md:top-[-100px] md:left-[-100px]`}
                             >
-                                {/* Horizontal line */}
-                                <div
-                                    className={` h-0
-                                        ${idx % 2 == 1 && 'right-0'} 
-                                        md:w-[49px] lg:w-[70px]
-                                        ${idx == events.length - 1 ? 'md:top-[-4.5px] lg:top-[-6.5px]' : 'md:top-[-8.5px] lg:top-[-12.5px]'}
-                                        md:border-[2.25px] lg:border-[3.2px] 
-                                        md:rounded-[1.4px] lg:rounded-[2px] 
-                                        border-[#FCFFCCCC] 
-                                        shadow-timeline absolute`}
-                                />
-                                <BigBlurredStarIcon className="absolute top-[-100px] left-[-100px]" />
-                                <BigEventCard
-                                    className={`absolute md:top-[-37px] lg:top-[-53px] 
-                                        ${idx % 2 == 0 ? 'md:left-[48px] lg:left-[68px]' : 'md:right-[48px] lg:right-[68px]'}`}
-                                    event={event}
-                                />
+                                <BigBlurredStarIcon className="max-md:hidden"/>
+                                <SmallBlurredStarIcon className="md:hidden"/>
                             </div>
-                        )
-                        :
-                        events.map((event, idx) =>
-                            <div
-                                key={event.name}
-                                className={`w-0 
-                                    ${idx == events.length - 1 ? 'h-0' : 'h-[60px] border-[2.5px]'} 
-                                    ${event.status === 'Done' ? 'border-[#FCFFCCCC]' : 'border-[#FCFFCC4D]'} 
-                                    shadow-timeline relative
-                                    ${(idx == events.length - 1) || (events[idx + 1].status === 'Awaiting') ? 'rounded-b-[2.5px]' : ''}`}
-                            >
-                                {/* Horizontal line */}
-                                <div
-                                    className={`w-[27px] h-0 
-                                        ${idx % 2 == 1 && 'right-0'} 
-                                        ${idx == events.length - 1 ? 'top-[-2px]' : 'top-[-4px]'} 
-                                        border-[1px] border-[#FCFFCCCC] rounded-[2.5px] 
-                                        shadow-timeline absolute`}
-                                />
-                                {/* <SmallCircleIcon className="absolute top-[-8.5px] left-[-10px]" /> */}
-                                <SmallBlurredStarIcon className="absolute top-[-30px] left-[-37px]" />
-                                <SmallEventCard
-                                    className={`absolute top-[-25px] 
-                                        ${idx % 2 == 0 ? 'left-[25px]' : 'right-[25px]'}`}
-                                    event={event}
-                                />
-                            </div>
-                        )
+                            <EventCard
+                                className={`absolute 
+                                    max-md:top-[-25px] md:top-[-37px] lg:top-[-53px] 
+                                    ${idx % 2 == 0 ? 'max-md:left-[25px] md:left-[48px] lg:left-[68px]' : 'max-md:right-[25px] md:right-[48px] lg:right-[68px]'}`}
+                                event={event}
+                            />
+                        </div>
+                    )
                 }
             </div>
 
             {/* Rabbit */}
             <div className="flex-1 flex flex-col gap-0 items-center">
                 <div className="relative">
-                    {
-                        isBigScreen ?
-                            <div className={`z-20 absolute 
-                                md:top-[-10vw] md:right-[-45vw] md:w-[168px] md:h-[202px] 
-                                lg:top-[-5vw] lg:right-[-45vw] lg:w-[280px] lg:h-[337px]`}
-                            >
-                                <Image src="/KelinciTimeline.png" alt="" width={280} height={337} />
-                            </div>
-                            :
-                            <div className="z-20 absolute top-[-10vw] right-[-45vw] w-[96px] h-[116px]">
-                                <Image src="/KelinciTimeline.png" alt="" width={96} height={116} />
-                            </div>
-                    }
+                    <div className={`z-20 absolute 
+                        max-md:right-[-45vw] max-md:top-[-10vw] max-md:w-[96px] max-md:h-[116px]
+                        md:right-[-45vw] md:top-[-10vw] md:w-[168px] md:h-[202px] 
+                        lg:right-[-45vw] lg:top-[-5vw] lg:w-[280px] lg:h-[337px]`}
+                    >
+                        <Image src="/KelinciTimeline.png" alt="" layout="fill" />
+                    </div>
                 </div>
             </div>
         </div>
--- src/modules/TimelineModule/module-elements/card.tsx
+++ src/modules/TimelineModule/module-elements/card.tsx
@@ -2,41 +2,22 @@ import { cn } from "@/lib/utils";
 import { EventEntry } from "../interface";
 
 
-export function BigEventCard({ className = "", event }: { className?: string, event: EventEntry }) {
+export function EventCard({ className = "", event }: { className?: string, event: EventEntry }) {
     return (
         <div 
             className={cn(
                 `z-10 
-                md:h-[70px] md:w-[175px] md:px-[16px] md:py-[15px] md:rounded-[15px] 
-                lg:h-[100px] lg:w-[250px] lg:px-[23px] lg:py-[19px] lg:rounded-[20px] 
-                flex flex-col md:gap-[4px] lg:gap-[12.88px] justify-center items-center 
+                max-md:h-[50px] max-md:w-[140px] max-md:px-[8.8px] max-md:py-[7.3px] max-md:gap-[6px] max-md:rounded-[7.8px]
+                md:h-[70px] md:w-[175px] md:px-[16px] md:py-[15px] md:gap-[4px] md:rounded-[15px] 
+                lg:h-[100px] lg:w-[250px] lg:px-[23px] lg:gap-[12.88px] lg:py-[19px] lg:rounded-[20px] 
+                flex flex-col justify-center items-center 
                 bg-[#6F4589] border shadow-event-entry-card`, 
                 className)}
         >
-            <h1 className="font-[900] font-cinzel md:max-lg:text-[16px] lg:text-[23px]">
+            <h1 className="font-[900] font-cinzel max-md:text-[14px] md:max-lg:text-[16px] lg:text-[23px]">
                 {event.date}
             </h1>
-            <h1 className="font-raleway font-[600] md:max-lg:text-[13px] lg:text-[19px]">
-                {event.name}
-            </h1>
-        </div>
-    )
-}
-
-
-export function SmallEventCard({ className = "", event }: { className?: string, event: EventEntry }) {
-    return (
-        <div 
-            className={cn(
-                `z-10 h-[50px] w-[140px] px-[8.8px] py-[7.3px] 
-                flex flex-col gap-[6px] justify-center items-center 
-                bg-[#6F4589] border shadow-event-entry-card-small rounded-[7.8px]`, 
-                className)}
-        >
-            <h1 className="font-[900] font-cinzel text-[14px]">
-                {event.date}
-            </h1>
-            <h1 className="font-raleway font-[600] text-[12px]">
+            <h1 className="font-raleway font-[600] max-md:text-[12px] md:max-lg:text-[13px] lg:text-[19px]">
                 {event.name}
             </h1>
         </div>
