8a6f5c1bba4b44a2d177340a546b953ab122c661
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-28 19:12:45 +0700

    fix: input rounded corner

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -31,7 +31,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <div className='flex flex-col gap-2'>
         {label && <Label>{label} {asterisk && <span className="text-red-500"> *</span>}</Label>}
-        <div className='flex items-center border-2 border-gray-500 rounded-[0.5rem] w-full text-black outline-none overflow-hidden relative'>
+        <div className='flex items-center border-2 border-gray-500 rounded-xl w-full text-black outline-none overflow-hidden relative'>
           <div className={cn('absolute', iconPosition === 'left' ? 'left-5' : 'right-5')}>
             {icon}
           </div>
@@ -40,7 +40,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             type={type}
             ref={ref}
             className={cn(
-              'w-full h-full rounded-[0.5rem] bg-white outline-none px-5 py-3 duration-300 text-t9 sm:text-t7',
+              'w-full h-full bg-white outline-none px-5 py-3 duration-300 text-t9 sm:text-t7',
               icon ? (iconPosition === 'left' ? 'pl-14' : 'pr-14') : '',
               className
             )}
