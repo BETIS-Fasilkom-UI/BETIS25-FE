1e0ad202a0040168083fb0bb196c39bba3dd0ce5
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-29 22:04:00 +0800

    feat: initialize profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,18 @@
+import { getUserData, getUserService } from "@/hooks/user";
 import { ProfilePageModule } from "@/modules/ProfilePageModule"
+import { redirect } from "next/navigation";
 
-const ProfilePage = () => {
-    return <ProfilePageModule />    
+const ProfilePage = async () => {
+    const user = await getUserData();
+
+    console.log(user);
+    
+
+    if (!user) {
+        redirect("/");
+    }
+
+    return <ProfilePageModule  user={user}/>    
 }
 
 export default ProfilePage
--- src/modules/ProfilePageModule/index.tsx
+++ src/modules/ProfilePageModule/index.tsx
@@ -1,7 +1,38 @@
-export const ProfilePageModule = () => {
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { User, UserJWT } from "@/hooks/interface"
+
+export const ProfilePageModule = ({ user }: {
+    user: User
+}) => {
     return (
-        <main>
-        
+        <main className="p-10 mt-40 ">
+            <section className="shadow-md rounded-lg p-6 ">
+                <Avatar className="w-40 h-40 mb-10">
+                    <AvatarImage
+                        src="https://github.com/shadcn.png"
+                        alt="@shadcn"
+                    />
+                    <AvatarFallback>
+                        {user.fullname[0].toUpperCase()}
+                    </AvatarFallback>
+                </Avatar>
+                <div className="mb-4">
+                    <h3 className="text-lg font-semibold">Nama Lengkap</h3>
+                    <p className="text-white/80">{user.fullname}</p>
+                </div>
+                <div className="mb-4">
+                    <h3 className="text-lg font-semibold">Nama Panggilan</h3>
+                    <p className="text-white/80">{user.nickname}</p>
+                </div>
+                <div className="mb-4">
+                    <h3 className="text-lg font-semibold">Email</h3>
+                    <p className="text-white/80">{user.email}</p>
+                </div>
+                <div className="mb-4">
+                    <h3 className="text-lg font-semibold">Nomor Telepon</h3>
+                    <p className="text-white/80">{user.phoneNumber}</p>
+                </div>
+            </section>
         </main>
     )
 }
