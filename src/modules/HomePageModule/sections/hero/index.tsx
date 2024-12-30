775c352f481d35ce985e145cffa7ffdfa393221b
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-30 13:03:22 +0800

    fix: remove empty box in hero

--- src/modules/HomePageModule/sections/hero/index.tsx
+++ src/modules/HomePageModule/sections/hero/index.tsx
@@ -6,40 +6,34 @@ import KupuKupu from "./assets/kupu";
 
 export default function Hero() {
   return (
-    <>
-      <div className="min-h-screen w-screen flex justify-center relative">
+    <div className="min-h-screen w-screen flex justify-center relative mb-[900px] max-md:mb-[650px] max-sm:mb-[400px]">
 
-        <div className="flex flex-col justify-center items-center">
-          <Greet
-            primaryText="BETIS 2025"
-            secondaryText="Bimbingan Belajar Gratis"
-            linkPrimary="Daftar Sekarang"
-            linkSecondary="Description"
-          />
-        </div>
-
-        <Jalan
-          className="z-[-1] w-[1000px] max-md:w-[900px] max-sm:w-[645px] aspect-[3197/4262] absolute top-[50%] translate-y-[-23%]"
-        />
-        <Kerajaan1
-          className="z-[-2] w-[1000px] max-md:w-[900px] max-sm:w-[645px] aspect-[4276/8320] absolute top-[15%] max-sm:top-[30%] left-0 opacity-80"
-        />
-        <Kerajaan2
-          className="z-[-2] w-[1000px] max-md:w-[900px] max-sm:w-[645px] aspect-[5760/8320] absolute top-[15%] max-sm:top-[30%] right-0 opacity-60"
-        />
-        <KupuKupu
-          reverse={false}
-          className="z-[-1] w-[200px] max-md:w-[80px] aspect-[800/678] absolute top-[20%] right-[3%]"
-        />
-        <KupuKupu
-          reverse={true}
-          className="z-[-1] w-[180px] max-md:w-[80px] aspect-[800/678] absolute top-[30%] left-[3%] transform scale-x-[-1]"
+      <div className="flex flex-col justify-center items-center">
+        <Greet
+          primaryText="BETIS 2025"
+          secondaryText="Bimbingan Belajar Gratis"
+          linkPrimary="Daftar Sekarang"
+          linkSecondary="Description"
         />
       </div>
 
-      <div className='h-[900px] max-md:h-[650px] max-sm:h-[400px]'>
-
-      </div>
-    </>
+      <Jalan
+        className="z-[-1] w-[1000px] max-md:w-[900px] max-sm:w-[645px] aspect-[3197/4262] absolute top-[50%] translate-y-[-23%]"
+      />
+      <Kerajaan1
+        className="z-[-2] w-[1000px] max-md:w-[900px] max-sm:w-[645px] aspect-[4276/8320] absolute top-[15%] max-sm:top-[30%] left-0 opacity-80"
+      />
+      <Kerajaan2
+        className="z-[-2] w-[1000px] max-md:w-[900px] max-sm:w-[645px] aspect-[5760/8320] absolute top-[15%] max-sm:top-[30%] right-0 opacity-60"
+      />
+      <KupuKupu
+        reverse={false}
+        className="z-[-1] w-[200px] max-md:w-[80px] aspect-[800/678] absolute top-[20%] right-[3%]"
+      />
+      <KupuKupu
+        reverse={true}
+        className="z-[-1] w-[180px] max-md:w-[80px] aspect-[800/678] absolute top-[30%] left-[3%] transform scale-x-[-1]"
+      />
+    </div>
   );
 };
