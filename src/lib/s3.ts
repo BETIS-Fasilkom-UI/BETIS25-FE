6d78936b2f5f5b0538a16785ca6e471490699f22
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-31 11:29:06 +0800

    feat: get asset from aws in s3

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -32,5 +32,12 @@ const uploadFile = async (file: File, key?: string, folder?: string) => {
     return data.url;
 }
 
-export { Bucket, s3, uploadFile };
+const getAsset = (key: string): string => { 
+    const cleanedKey = key.replace(/^\/|\/$/g, '');
+    const url = `https://${Bucket}.s3.amazonaws.com/assets/${cleanedKey}`;
+
+    return url;
+}
+
+export { Bucket, s3, uploadFile, getAsset};
 
