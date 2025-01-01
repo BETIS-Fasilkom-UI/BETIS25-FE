914147b2840bb1808c5ea342d5f1e7b61f5dae08
Tristan A. <tristanagraa@gmail.com>
2025-01-02 05:31:07 +0700

    fix: lentera hero

--- src/modules/HomePageModule/sections/hero/assets/mascot.tsx
+++ src/modules/HomePageModule/sections/hero/assets/mascot.tsx
@@ -9,11 +9,11 @@ export default function Mascot(props: {className: string}) {
         <div className={`${props.className}`}>
             <div className='w-full h-full relative'>
                 <Lentera
-                    className={`${styles.rotateLentera} z-[0] w-[20%] aspect-[309/583] absolute right-[1%] top-[14%]`}
+                    className={`${styles.rotateLentera} z-[0] w-[20%] aspect-[309/583] absolute -right-[4.5%] top-[14%]`}
                 />
                 <Image
                     className='z-0'
-                    src={getAsset("/orangbawalentera.png")} 
+                    src={getAsset("/Mascot.png")} 
                     alt="mascot" 
                     sizes="none"
                     fill
