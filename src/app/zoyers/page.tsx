a54ea4124e25589484b386a5f88604d0026f0c36
szoyaf <shaneyzoyaf@gmail.com>
2025-01-18 01:08:44 +0700

    feat: remove hardcode page

--- src/app/zoyers/page.tsx
+++
@@ -1,9 +0,0 @@
-import SubmissionTemp from "@/modules/SubmissionTempModule";
-
-export default function Home() {
-  return (
-    <div className="flex justify-center flex-col gap-1 pt-16 items-center overflow-hidden">
-      <SubmissionTemp />
-    </div>
-  );
-}
