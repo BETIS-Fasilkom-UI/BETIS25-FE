40eb0a5c458df53d693e2e18487e99a70668e37f
Tristan A. <tristanagraa@gmail.com>
2025-01-26 03:07:21 +0700

    feat: courselist route

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,14 +1,27 @@
 import React from "react";
-import CourseList from "@/modules/CourseListModule/CourseList";
+import CourseList from "@/modules/CourseListModule/";
 import { getUserService } from "@/hooks/user";
 import { redirect } from "next/navigation";
+import { ApiResponse } from "@/hooks/interface";
+import { CourseInterface } from "@/modules/CourseListModule/interface";
 
 const page = async () => {
-  //const user = await getUserService();
-  //if (user) {
-  //  redirect("/login");
-  //}
-  return <CourseList />;
+  const user = await getUserService();
+  if (!user) {
+    redirect("/login");
+  }
+
+  const data = await fetch(process.env.SERVER_URL + "course", {
+    method: "GET",
+    credentials: "omit",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${user.token}`,
+    },
+  });
+  const response: ApiResponse<CourseInterface[]> = await data.json();
+  console.log(response);
+  return <CourseList courses={response.data} />;
 };
 
 export default page;
