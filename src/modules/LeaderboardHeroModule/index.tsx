f7da3b76bd81753d657f7b69bf10a09357dc9bf6
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-02-08 23:45:42 +0700

    feat: Added interfaces for podium data

--- src/modules/LeaderboardHeroModule/index.tsx
+++ src/modules/LeaderboardHeroModule/index.tsx
@@ -1,45 +1,46 @@
 import { FirstAvatar, FirstAvatarMobile, SecondAvatar, SecondAvatarMobile, ThirdAvatar, ThirdAvatarMobile } from "./components/PodiumAvatar"
+import { LeaderboardHeroProps } from "./interfaces"
 
 
-export const LeaderboardHeroDesktop = ({}) => {
+export const LeaderboardHeroDesktop = ({ first, second, third }: LeaderboardHeroProps) => {
     return (
         <div className="lg:flex-1 flex lg:flex-row max-lg:hidden w-screen justify-center items-center h-full xl:gap-[110px]">
             <div className="w-[380px] h-[650px] pt-[6rem] flex justify-center">
-                <SecondAvatar name="NAOMI SHAKILA ISBONO" institute="SMA Labschool Cirendeu" score="750" avatarSrc="/Pp-girl2.png"/>
+                <SecondAvatar {...second} />
             </div>
             <div className="w-[380px] h-[650px] flex justify-center">
-                <FirstAvatar name="NAOMI SHAKILA ISBONO" institute="SMA Labschool Cirendeu" score="750" avatarSrc="/Pp-girl2.png"/>
+                <FirstAvatar {...first} />
             </div>
             <div className="w-[380px] h-[650px] pt-[6rem] flex justify-center">
-                <ThirdAvatar name="NAOMI SHAKILA ISBONO" institute="SMA Labschool Cirendeu" score="750" avatarSrc="/Pp-girl2.png"/>
+                <ThirdAvatar {...third} />
             </div>
         </div>
     )
 }
 
 
-export const LeaderboardHeroMobile = ({}) => {
+export const LeaderboardHeroMobile = ({ first, second, third }: LeaderboardHeroProps) => {
     return (
         <div className="max-lg:flex-1 flex flex-col lg:hidden w-screen justify-center items-center gap-[30px]">
             <div className="w-[380px] h-[350px] flex justify-center">
-                <FirstAvatarMobile name="NAOMI SHAKILA ISBONO" institute="SMA Labschool Cirendeu" score="750" avatarSrc="/Pp-girl2.png"/>
+                <FirstAvatarMobile {...first} />
             </div>
             <div className="w-[380px] h-[350px] flex justify-center">
-                <SecondAvatarMobile name="NAOMI SHAKILA ISBONO" institute="SMA Labschool Cirendeu" score="750" avatarSrc="/Pp-girl2.png"/>
+                <SecondAvatarMobile {...second} />
             </div>
             <div className="w-[380px] h-[350px] flex justify-center">
-                <ThirdAvatarMobile name="NAOMI SHAKILA ISBONO" institute="SMA Labschool Cirendeu" score="750" avatarSrc="/Pp-girl2.png"/>
+                <ThirdAvatarMobile {...third} />
             </div>
         </div>
     )
 }
 
 
-export const LeaderboardHeroModule = ({}) => {
+export const LeaderboardHeroModule = (props: LeaderboardHeroProps) => {
     return (
-    <>
-        <LeaderboardHeroDesktop/>
-        <LeaderboardHeroMobile/>
-    </>
+        <>
+            <LeaderboardHeroDesktop {...props} />
+            <LeaderboardHeroMobile {...props} />
+        </>
     )
 }
