f419404cd71f1ab80cd5893a2445c13b56ead68c
szoyaf <shaneyzoyaf@gmail.com>
2025-02-09 17:44:58 +0700

    feat: bg hero webinar ppkb

--- src/modules/WebinarPPKBHeroModule/index.tsx
+++ src/modules/WebinarPPKBHeroModule/index.tsx
@@ -1,14 +1,16 @@
 import CountdownHero from "@/components/elements/CountdownHero";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { getAsset } from "@/lib/s3";
+import Image from "next/image";
 
 const WebinarPPKBHeroModule = () => {
   return (
-    <Card className="flex flex-col justify-center items-center bg-[#F8EBF3] bg-opacity-[20%] shadow-[5px_5px_20px_rgba(254,245,255,1),-5px_-5px_20px_rgba(254,245,255,1)] sm:shadow-[10px_10px_30px_rgba(254,245,255,1),-10px_-10px_30px_rgba(254,245,255,1)] md:shadow-[10px_10px_40px_rgba(254,245,255,1),-10px_-10px_40px_rgba(254,245,255,1)] w-[85%] md:w-[80%] h-fit rounded-[51.64px] sm:rounded-[59.25px] lg:rounded-[69.25px] xl:rounded-[89.25px]">
+    <Card className="flex flex-col overflow-hidden justify-center items-center bg-[#F8EBF3] bg-opacity-[20%] shadow-[5px_5px_20px_rgba(254,245,255,1),-5px_-5px_20px_rgba(254,245,255,1)] sm:shadow-[10px_10px_30px_rgba(254,245,255,1),-10px_-10px_30px_rgba(254,245,255,1)] md:shadow-[10px_10px_40px_rgba(254,245,255,1),-10px_-10px_40px_rgba(254,245,255,1)] w-[85%] md:w-[80%] h-fit rounded-[51.64px] sm:rounded-[59.25px] lg:rounded-[69.25px] xl:rounded-[89.25px]">
       <CardTitle className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl mt-5">
         Webinar ppkb
       </CardTitle>
-      <CardContent className="w-[100%] sm:w-[80%] md:w-[80%] lg:w-[75%] xl:w-[63%] flex flex-col justify-center items-center gap-[35px]">
+      <CardContent className="w-[100%] sm:w-[80%] md:w-[80%] lg:w-[75%] xl:w-[63%] h-fit flex flex-col justify-center items-center gap-[35px]">
         <div className="flex flex-col gap-[32px]">
           <CountdownHero
             displayDate
@@ -27,6 +29,65 @@ const WebinarPPKBHeroModule = () => {
         >
           Register Here
         </Button>
+
+        <div className="absolute max-md:hidden -z-20 sm:h-full md:h-full lg:h-5/6 xl:h-full left-0 sm:-bottom-[120px] md:-bottom-[110px] lg:-bottom-[68px] xl:-bottom-[95px] sm:aspect-[1/2] md:aspect-[5/8] lg:aspect-[4/5] xl:aspect-[7/9]">
+          <Image
+            alt="BG"
+            src={getAsset("/ppkbkiri.png")}
+            fill
+            sizes="none"
+            className="object-contain"
+          />
+        </div>
+        <div className="absolute max-md:hidden -z-20 sm:h-2/3 md:h-full lg:h-5/6 xl:h-full right-0 sm:-bottom-[37px] md:-bottom-[95px] lg:-bottom-9 xl:-bottom-10 sm:aspect-[1/2] md:aspect-[3/7] lg:aspect-[3/5] xl:aspect-[3/5]">
+          <Image
+            alt="BG"
+            src={getAsset("/ppkbkanan.png")}
+            fill
+            sizes="none"
+            className="object-contain"
+          />
+        </div>
+
+        <div className="absolute -z-30 max-sm:-translate-x-[110px] h-[560px] sm:h-[600px] md:h-[500px] lg:h-[600px] xl:h-[650px] bottom-0 aspect-[5/3]">
+          <Image
+            alt="BG"
+            src={getAsset("/castle.png")}
+            fill
+            sizes="none"
+            className="object-contain"
+          />
+        </div>
+
+        <div className="relative md:hidden -z-20 h-[270px] sm:h-[330px] -mb-[20px] -mt-[140px] sm:-mt-[150px] aspect-[1/1]">
+          <Image
+            alt="BG"
+            src={getAsset("/ppkbtengah.png")}
+            fill
+            sizes="none"
+            className="object-contain"
+          />
+        </div>
+
+        <div className="absolute -z-20 h-[80px] sm:h-[100px] md:h-[130px] lg:h-[150px] xl:h-[200px] -left-0 md:-left-[4px] -top-[4px] md:-top-[7px] lg:-top-[8px] aspect-[3/1]">
+          <Image
+            alt="BG"
+            src={getAsset("/lampukiri.png")}
+            fill
+            sizes="none"
+            className="object-contain"
+          />
+        </div>
+
+        <div className="absolute -z-20 h-[80px] sm:h-[100px] md:h-[130px] lg:h-[150px] xl:h-[200px] -right-0 -top-[10px] md:-top-[12px] lg:-top-[19px] aspect-[3/1]">
+          <Image
+            alt="BG"
+            src={getAsset("/lampukanan.png")}
+            fill
+            sizes="none"
+            className="object-contain"
+          />
+        </div>
       </CardContent>
     </Card>
   );
