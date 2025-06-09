5cd24d8a588c6b3e06d652b7336c972929f663e8
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-06-09 16:36:17 +0700

    fix: pnpm lock and sponsor

--- src/modules/HomePageModule/sections/Sponsor.tsx
+++ src/modules/HomePageModule/sections/Sponsor.tsx
@@ -1,35 +1,26 @@
-import React from 'react';
-import { Card } from '@/components/ui/card';
 import Image from 'next/image';
 
 const Sponsor = () => {
   return (
-    <div className="flex flex-col items-center space-y-10 mt-24 lg:mt-48">
-      {/* Title */}
-      <h1 className="text-2xl md:text-4xl lg:text-5xl font-cinzel text-white">
+    <article className="mt-32">
+      <h1 className="text-xl lg:text-3xl font-cinzel text-center">
         Our Sponsors
       </h1>
-
-      {/* Sponsor Card Container */}
-      <Card className="bg-[#6f4589] rounded-[32px] shadow-[4px_4px_12px_0px_rgba(254,245,255,0.40),  4px_-4px_12px_0px_rgba(254,245,255,0.40)] w-[95vw] md:w-[90vw] lg:w-[80vw]">
-        <div className="flex flex-col items-center space-y-8 sm:space-y-4">
-          <div className='flex gap-2'>
-            <Image
-              alt='Sponsor Logo Hotways'
-              src='https://betis25.s3.ap-southeast-2.amazonaws.com/sponsors/hotways.jpg'
-              fill
-              className="object-contain"
-            />
-            <Image
-              alt='Sponsor Logo Sido'
-              src='https://betis25.s3.ap-southeast-2.amazonaws.com/sponsors/sidomuncul-transparent.png'
-              fill
-              className="object-contain"
-            />
-          </div>
-        </div>
-      </Card>
-    </div>
+      <div className="w-full bg-white p-10 rounded-3xl mt-10 flex gap-5 flex-wrap items-center justify-center">
+        <Image
+          src={'/sido.png'}
+          width={200}
+          height={300}
+          alt="sido"
+        />
+        <Image
+          src={'/hotways.jpg'}
+          width={200}
+          height={200}
+          alt="hotways"
+        />
+      </div>
+    </article>
   );
 };
 
